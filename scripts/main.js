329a45235595c21b4c5e2f78b3bd095b87dd530f
isidrink <isidro.escoda@gmail.com>
2013-10-21 12:58:53 +0200

    Prueba 38

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,24 @@
-document.addEventListener("deviceready", onDeviceReady, false);
-//Activate :active state
-document.addEventListener("touchstart", function() {
-}, false);
+var app;
 
-function onDeviceReady() {
-	navigator.splashscreen.hide();
-	document.getElementById('btnOpenPDF').onclick = function() {
-		var app = new Application();
-		app.Run();
-	}
-}
+require.config({
+	paths: {
+	    jQuery: "../kendo/js/jquery.min",
+		kendo: "../kendo/js/kendo.mobile.min",
+        kendoIndexedListView: "../kendo/js/kendo.indexedlistview"
+	},
+    shim: {
+        jQuery: {
+            exports: "jQuery"
+        },
+        kendo: {
+            exports: "kendo"
+        }
+    }
+});
 
-function Application() {
-}
-
-Application.prototype.Run = function() {
-	if (device.uuid == "e0101010d38bde8e6740011221af335301010333" || device.uuid == "e0908060g38bde8e6740011221af335301010333") {
-		alert("Not Supported in Simulator.");
-	}
-	else {
-		var infoDiv = document.getElementById("infoField");
-		var path = this.getWorkingFolder().replace('http://', 'file://') + "sample.pdf";
-		infoDiv.innerText = path;
-        
-		if (device.platform === 'Android') {
-			window.open(path, '_system');
-		}
-		else {
-			window.open(path, '_blank');
-		}
-	}
-}
-
-Application.prototype.getWorkingFolder = function() {
-	var path = window.location.href.replace('index.html', '');
-	return path;
-}
+require(["jQuery", "app"], function($, application) {
+    $(function() {
+        app = application
+        application.init();
+    });
+});
